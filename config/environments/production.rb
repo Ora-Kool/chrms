a4e59e168914ddd055b86a35d70d411a00e77d87
Ora-Kool <etingemabian@gmail.com>
2016-11-25 13:07:45 +0100

    nothing but bugs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,11 +61,12 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :stmp
+  config.action_mailer.delivery_method = :smtp
   host = "chrms.herokuapp.com"
-  config.action_mailer.default_url_options = { host: host }
-ActionMailer::Base.smtp_settings = {
+  config.action_mailer.default_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
